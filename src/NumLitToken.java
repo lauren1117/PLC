6db3d2909a170be62f75b06d691190e956a1c738
Julie Maricic-Detweiler <j.maricicdetweil@ufl.edu>
2023-02-04 14:48:28 -0500

    Resolved errors; moved NumLit class into own file

---
+++ src/NumLitToken.java
@@ -0,0 +1,23 @@
+package edu.ufl.cise.plcsp23;
+
+class NumLitToken implements INumLitToken {
+
+    public int getValue() {
+        return 0;
+    }
+
+    @Override
+    public SourceLocation getSourceLocation() {
+        return null;
+    }
+
+    @Override
+    public Kind getKind() {
+        return null;
+    }
+
+    @Override
+    public String getTokenString() {
+        return null;
+    }
+}
